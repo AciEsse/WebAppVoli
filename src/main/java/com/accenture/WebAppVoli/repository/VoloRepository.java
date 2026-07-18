5bd09f15b4cb2bb58545b40ea29fbd652ebbfa36
andreascalzo99 <a.scalzo3@studenti.uniba.it>
2026-07-18 21:37:27 +0200

    aggiunta endpoint

--- src/main/java/com/accenture/WebAppVoli/repository/VoloRepository.java
+++ src/main/java/com/accenture/WebAppVoli/repository/VoloRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accenture.WebAppVoli.model.Volo;
+import java.time.LocalTime;
+
 
 public interface VoloRepository extends JpaRepository<Volo, Integer>{
     List<Volo> findByAeroportoPartenza(String aeroportoPartenza);
@@ -15,4 +17,6 @@ public interface VoloRepository extends JpaRepository<Volo, Integer>{
     List<Volo> findByCompagnia(String compagnia);
 
     List<Volo> findByData(LocalDate data);
+
+    //List<Volo> findByOrarioDecollo(LocalTime orarioDecollo);
 }
--- src/main/java/com/accenture/WebAppVoli/service/VoloService.java
+++ src/main/java/com/accenture/WebAppVoli/service/VoloService.java
@@ -87,5 +87,4 @@ public class VoloService {
 
         return voloRepository.findAll();
     }
-
 }
